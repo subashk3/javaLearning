e82e975d01a1b27357551703d8b5265cdefe1f38
subashk3 <subashktsmpi@gmail.com>
2022-02-05 13:54:51 +0530

    Calculator Program completed

---
+++ src/testprograms/Calculator.java
@@ -0,0 +1,58 @@
+package testprograms;
+
+import java.util.Scanner;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int f, s;
+		char o;
+
+		System.out.println("********This is calculator********");
+
+		System.out.println("Enter the first Number:");
+		f = sc.nextInt();
+
+		System.out.println("Enter the second Number:");
+		s = sc.nextInt();
+
+		System.out.println("Enter the operator (+ - * / )");
+
+		o = sc.next().charAt(0);
+		
+		
+		int res = calculator(f,s,o);
+		
+		System.out.println("Result is: "+ res);
+
+	}
+
+	static int calculator(int a, int b, char c) {
+
+		switch (c) {
+
+		case '+':
+			return a + b;
+			
+		case '-':
+			return a - b;
+			
+		case '*':
+			return a * b;
+			
+		case '/':
+			return a / b;
+			
+		default:
+		System.out.println("Enter Wrong Operator check again:");		
+			
+			
+
+		}
+		return 0;
+
+	}
+
+}
