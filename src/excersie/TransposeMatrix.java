ddb110b2c746f615538ccb9d7f6fa575c092eb29
subashk3 <subashktsmpi@gmail.com>
2022-02-07 13:47:44 +0530

    Upload

---
+++ src/excersie/TransposeMatrix.java
@@ -0,0 +1,45 @@
+//Program to Find Transpose of a Matrix
+package excersie;
+
+import java.util.Scanner;
+
+public class TransposeMatrix {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int[][] firstMatrix;
+		int[][] TransposeMatrix;
+		int row, col, trow, tcol;
+		System.out.println("Enter the Number of rows:");
+		row = sc.nextInt();
+		System.out.println("Enter the Number of rows:");
+		col = sc.nextInt();
+		trow = col;
+		tcol = row;
+
+		firstMatrix = new int[row][col];
+		TransposeMatrix = new int[trow][tcol];
+
+		System.out.println("Enter firstMatrix");
+		for (int i = 0; i < row; i++) {
+			for (int j = 0; j < col; j++) {
+				firstMatrix[i][j] = sc.nextInt();
+			}
+		}
+
+		System.out.println();
+		for (int i = 0; i < row; i++) {
+			for (int j = 0; j < col; j++) {
+				TransposeMatrix[i][j] = firstMatrix[j][i];
+			}
+		}
+		System.out.println("Transpose is :");
+		for (int i = 0; i < row; i++) {
+			for (int j = 0; j < col; j++) {
+				System.out.print(TransposeMatrix[i][j] + " ");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
