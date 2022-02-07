314c3c47eba7b3021b2f60be9aca3e8b5932bfc1
subashk3 <subashktsmpi@gmail.com>
2022-02-07 15:33:48 +0530

    Update pattern of %

---
+++ src/testprograms/PatternCh.java
@@ -0,0 +1,30 @@
+package testprograms;
+
+public class PatternCh {
+
+	public static void main(String[] args) {
+
+		int n = 5;
+
+		for (int i = 1; i <= n; i++) {
+
+			for (int j = 1; j <= n - i; j++) {
+
+				System.out.print("@ ");
+			}
+			for (int k = 1; k < 2 * i; k++) {
+
+				System.out.print("% ");
+			}
+
+			for (int z = 1; z <= n - i; z++) {
+
+				System.out.print("@ ");
+			}
+
+			System.out.println();
+
+		}
+
+	}
+}
