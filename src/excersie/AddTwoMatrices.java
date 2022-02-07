48183c8a75ac55d5c44661d05c9cc6d4c6bae435
subashk3 <subashktsmpi@gmail.com>
2022-02-07 13:33:48 +0530

    exercise upload

--- src/excersie/AddTwoMatrices.java
+++ src/excersie/AddTwoMatrices.java
@@ -7,21 +7,45 @@ public class AddTwoMatrices {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		int[][] firstMatrix;
+		int[][] secondMatrix;
+		int[][] resulMatrix;
+		int row, col;
+		System.out.println("Enter the Number of rows:");
+		row = sc.nextInt();
+		System.out.println("Enter the Number of rows:");
+		col = sc.nextInt();
+		firstMatrix = new int[row][col];
+		secondMatrix = new int[row][col];
+		resulMatrix = new int[row][col];
 
-		int firtMatrice[][] = { { 1, 2, 3 }, { 4, 5, 6 }, { 1, 2, 3 } };
-		int secondMatrice[][] = { { 1, 2, 3 }, { 4, 5, 6 }, { 1, 2, 3 } };
-		int res[][] = new int[3][3];
-		int temp[][] = new int[3][3];
-		System.out.println(firtMatrice.length);
-		
-		for(int i=0;i<firtMatrice.length;i++) {
-			
-			for(int j=0;j<=secondMatrice.length;j++) {
-				
-				
-				
-				
+		System.out.println("Enter firstMatrix");
+		for (int i = 0; i < row; i++) {
+			for (int j = 0; j < col; j++) {
+				firstMatrix[i][j] = sc.nextInt();
 			}
+		}
+		System.out.println("Enter Second tMatrix");
+		for (int i = 0; i < row; i++) {
+			for (int j = 0; j < col; j++) {
+				secondMatrix[i][j] = sc.nextInt();
+			}
+		}
+
+		System.out.println();
+		for (int i = 0; i < row; i++) {
+			for (int j = 0; j < col; j++) {
+
+				resulMatrix[i][j] = firstMatrix[i][j] + secondMatrix[i][j];
+
+			}
+			}
+		System.out.println("Resu is :");
+		for (int i = 0; i < row; i++) {
+			for (int j = 0; j < col; j++) {
+				System.out.print(resulMatrix[i][j] + " ");
+			}
+			System.out.println();
 		}
 
 	}
--- src/testprograms/Teee.java
+++ src/testprograms/Teee.java
@@ -1,15 +1,32 @@
 package testprograms;
 
+import java.util.Scanner;
+
 public class Teee {
 
-	
-	public static void main(String [] args) {
-		
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		// test programs
-		
-		
-	
+
+		int[][] a = new int[2][2];
+
+		for (int i = 0; i < 2; i++) {
+
+			for (int j = 0; j < 2; j++) {
+
+				a[i][j] = sc.nextInt();
+
+			}
+		}
+
+		for (int i = 0; i < 2; i++) {
+
+			for (int j = 0; j < 2; j++) {
+
+				System.out.println(a[i][j]+ " ");
+			}
+		}
+
 	}
-	
-	
+
 }
