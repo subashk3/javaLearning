b452b1e2e0ccd5b6945c91141896b046c4e66e40
subashk3 <subashktsmpi@gmail.com>
2022-02-08 10:18:31 +0530

    Upload

---
+++ src/excersie/ReverseSentence.java
@@ -0,0 +1,35 @@
+//63.program to Reverse a Sentence Using Recursion
+package excersie;
+
+import java.util.Scanner;
+
+public class ReverseSentence {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		String inputString = new String();
+		System.out.println("Enter the string: ");
+		inputString = sc.nextLine();
+
+		String replaceString = "";
+
+		int index = inputString.length();
+
+		// Changing letter using index value
+		for (int i = index - 1; i >= 0; i--) {
+			replaceString += inputString.charAt(i);
+		}
+		System.out.println("Normal Method: " + replaceString);
+
+		System.out.println("Recursion Method: " + reverse(inputString));
+
+	}
+
+	public static String reverse(String sentence) {
+		if (sentence.isEmpty())
+			return sentence;
+
+		return reverse(sentence.substring(1)) + sentence.charAt(0);
+	}
+}
