c9cb982b5d458d844fafdc148c5c295a4f22f1e4
subashk3 <subashktsmpi@gmail.com>
2022-02-08 14:57:04 +0530

    Upload

---
+++ src/excersie/ThreeZero.java
@@ -0,0 +1,22 @@
+package excersie;
+
+public class ThreeZero {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println(
+				"The Java long keyword is a primitive data type.\nIt can hold a 64-bit two's complement integer.\nIts default value is 0L.\nIts default size is 8 byte.");
+
+		System.out.println("Max Value of Long :" + Long.MAX_VALUE);
+
+		System.out.println("Min Value of Long :" + Long.MIN_VALUE);
+		
+		// Declaration and initilization
+
+		long l = 70103731710L;
+
+		System.out.println(l);
+	}
+
+}
