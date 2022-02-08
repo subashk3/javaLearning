350bae9f9cd44803a8724759adbe5b23dd8da21d
subashk3 <subashktsmpi@gmail.com>
2022-02-08 16:50:23 +0530

    Upload

---
+++ src/everyday/StringClass.java
@@ -0,0 +1,65 @@
+//String is Non-primitive datatype
+//String is special class
+//String is immutable 
+
+package everyday;
+
+public class StringClass {
+
+	public static void main(String[] args) {
+		// Object declaration in heap memory
+		String s1 = new String("Hello this first Program from String");
+
+		// literal declaration in String Constant Pool memory
+		String s2 = " don't judge a book by its cover ";
+
+		/* String methods; */
+
+		// lenth() - calculate length
+		// System.out.println(s1.length());
+
+		// isBlank() - return boolean value
+		// s2 = " ";
+		// System.out.println(s2.isBlank());
+
+		// isEmpty(); -return boolean value
+		// s2 = " ";
+		// s2 = "";
+		// System.out.println(s2.isEmpty());
+
+		// equals() - return boolean value
+		// System.out.println(s2.equals(s1));
+
+		/* String searching methods */
+
+		// indexOf();
+//		System.out.println("Frist char index"+s1.indexOf('a'));
+//		System.out.println(s1.indexOf("this"));
+
+		// charAt(index);
+		// System.out.println(s2.charAt(5));
+
+		// substring(index);
+		// System.out.println(s2.substring(2));
+		// System.out.println(s2.substring(2, 6));
+
+		// strp() - Unicode support
+		// System.out.println(s2.strip());
+
+		// trim() - Unicode not support
+
+		// System.out.println(s2.trim());
+
+		// Repeat(count)
+		// System.out.println(s2.repeat(5));
+
+		// toUpperCase() toLowerCase()
+//		s2 = "aBcde";
+//		System.out.println(s2.toLowerCase()+ " " +s2.toUpperCase());
+		
+		
+		
+
+	}
+
+}
---
+++ src/excersie/PolynomialEquation.java
@@ -0,0 +1,47 @@
+//15) Write a program to evaluate the polynomial equation
+package excersie;
+
+import java.util.Scanner;
+
+public class PolynomialEquation {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Number of A:");
+		double firstNum = sc.nextDouble();
+		System.out.println("Enter Number of B:");
+		double secondNum = sc.nextDouble();
+		System.out.println("Enter Number of C:");
+		double thirdNum = sc.nextDouble();
+
+		polynomialEquation(firstNum, secondNum, thirdNum);
+
+	}
+
+	static void polynomialEquation(double a, double b, double c) {
+
+		double discriminal = b * b - (4 * a * c);
+
+		if (discriminal > 0) {
+
+			double num1 = (-b + Math.sqrt(discriminal)) / (2 * a);
+			double num2 = (-b - Math.sqrt(discriminal)) / (2 * a);
+
+			System.out.println("Result values are: " + num1 + ", " + num2);
+
+		} else if (discriminal == 0) {
+
+			double num = -b / (2 * a);
+			System.out.println("Result value is: " + num);
+		} else {
+
+			double temp1 = -b / (2 * a);
+			double temp2 = (Math.sqrt((-b * b) - (4 * a * c))) / (2 * a);
+
+			System.out.println("Result is : " + temp1 + "" + temp2 + "i");
+		}
+
+	}
+
+
+}
