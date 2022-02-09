cb58ec785c26a3260f12a3b1cb50f4a15d45b7f9
subashk3 <subashktsmpi@gmail.com>
2022-02-09 12:41:38 +0530

    Correction updated

--- src/excersie/Binary2Decimal.java
+++ src/excersie/Binary2Decimal.java
@@ -4,7 +4,8 @@ package excersie;
 import java.util.Scanner;
 
 public class Binary2Decimal {
-	static int countNum(int a) {
+	// calculate count of the integer
+	public static int countNum(int a) {
 		int count = 0;
 		do {
 			count++;
@@ -13,27 +14,61 @@ public class Binary2Decimal {
 		return count;
 	}
 
-	static void binary2Decimal(int a) {
+//binary to Decimal
+	public int binary2Decimal(int a) {
 		int count = countNum(a);
 		int res = 0, temp;
 		for (int i = 0; i < count; i++) {
 			temp = a % 10;
 			a = a / 10;
-
 			// System.out.println(temp + " " + i);
 			res = res + (int) ((temp * Math.pow(2, i)));
 		}
-		System.out.println(res);
+		return res;
+	}
+
+//Decimal to binary
+	public int decimal2Binary(int getnumber) {
+
+		int reminder;
+		String stringinput = "";
+		while (getnumber != 0) {
+			reminder = getnumber % 2;
+			stringinput = stringinput + reminder;
+			getnumber /= 2;
+		}
+		// temperay variable
+		String temp = "";
+
+		// reverse the string
+		for (int i = stringinput.length() - 1; i >= 0; i--) {
+			temp += stringinput.charAt(i);
+		}
+
+		// System.out.println(temp);
+		int binary = Integer.parseInt(temp);
+
+		return binary;
 	}
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		Binary2Decimal obj = new Binary2Decimal();
 
-		System.out.println("Enter the Binary Number (0 and 1):");
+		System.out.println("Enter your choise");
+		System.out.println("1. Binary Number to Decimal");
+		System.out.println("2. Decimal Number to Binary");
+		int choise = sc.nextInt();
+		if (choise == 1) {
+			System.out.println("Enter the Binary Number (0 and 1):");
+			int getnum = sc.nextInt();
+			System.out.println(obj.binary2Decimal(getnum));
+		} else if (choise == 2) {
+			System.out.println("Enter the Decimal Number:");
+			int getnumtwo = sc.nextInt();
+			System.out.println(obj.decimal2Binary(getnumtwo));
 
-		int getNumb = sc.nextInt();
-
-		binary2Decimal(getNumb);
+		}
 
 	}
 
--- src/testprograms/Teee.java
+++ src/testprograms/Teee.java
@@ -3,30 +3,34 @@ package testprograms;
 import java.util.Scanner;
 
 public class Teee {
+	public int decimal2Binary(int getnum) {
+		int rem, decimalnumber = 0;
+		String s1 = "";
+		while (getnum != 0) {
+			rem = getnum % 2;
+			if (rem == 1 || rem == 0) {
+				s1 = s1 + rem;
+			}
+			getnum /= 2;
+		}
+
+		String temp = "";
+		for (int i = 0; i < s1.length(); i++) {
+
+		}
+		for (int i = s1.length() - 1; i >= 0; i--) {
+			temp += s1.charAt(i);
+		}
+		System.out.println(temp);
+		;
+		int k = Integer.parseInt(temp);
+		return k;
+	}
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		// test programs
-
-		int[][] a = new int[2][2];
-
-		for (int i = 0; i < 2; i++) {
-
-			for (int j = 0; j < 2; j++) {
-
-				a[i][j] = sc.nextInt();
-
-			}
-		}
-
-		for (int i = 0; i < 2; i++) {
-
-			for (int j = 0; j < 2; j++) {
-
-				System.out.println(a[i][j]+ " ");
-			}
-		}
-
+		Teee ob = new Teee();
+		ob.decimal2Binary(24);
 	}
 
 }
