7f599645e6297c0fd05a6680adb91b7bb9c7cb22
subashk3 <subashktsmpi@gmail.com>
2022-02-18 16:16:41 +0530

    upload

--- src/everyday/Hashset.java
+++ src/everyday/Hashset.java
@@ -2,6 +2,7 @@ package everyday;
 
 import java.util.HashSet;
 import java.util.LinkedHashSet;
+import java.util.TreeSet;
 
 import javax.swing.text.html.HTMLDocument.Iterator;
 
@@ -10,22 +11,25 @@ public class Hashset {
 	public static void main(String[] args) {
 		/// LinkedHashSet hs = new LinkedHashSet();
 		HashSet hs = new HashSet();
-		hs.add("Hello");
-		hs.add("Subash");
-		hs.add("Subash");
-		hs.add("Chandra");
-		hs.add("Bose");
-		hs.add("Bose");
-		
+		hs.add(12);
+		hs.add(54);
+		hs.add(14);
+		hs.add(198);
+		hs.add(1);
+		hs.add(1);
 
 		System.out.println(hs);
 		
+		TreeSet tr = new TreeSet(hs);
+
+		System.out.println(tr);
+
 		// String[] ar = (String[]) hs.clone();
 
 		// System.out.println(ar.toString());
 
 		// Iterator it = (Iterator) hs.iterator();
-		
+
 		// System.out.println(hs.size());
 
 	}
