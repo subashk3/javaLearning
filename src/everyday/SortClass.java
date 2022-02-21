7f94dd905848654516b1352bb7c86cbd7af50839
subashk3 <subashktsmpi@gmail.com>
2022-02-21 06:37:26 +0530

    Upload

--- src/everyday/SortClass.java
+++ src/everyday/SortClass.java
@@ -21,13 +21,31 @@ public class SortClass {
 			System.out.print(k + " ");
 
 	}
-
 	public static void main(String[] args) {
 		int a[] = { 40, 7, 5, 1 };
 		int n = a.length;
 
 		SortClass o = new SortClass();
-		//o.bubbleSorting(a, n);
+		// o.bubbleSorting(a, n);
+		o.selectionSorting(a, n);
+	}
+	public void selectionSorting(int[] a, int n) {
+		int min;
+		for (int i = 0; i < n - 1; i++) {
+			min = i;
+			for (int j = i + 1; j < n; j++) {
+				if (a[min] > a[j]) {
+					min = j;
+				}
+			}
+			int temp = a[min];
+			a[min] = a[i];
+			a[i] = temp;
+
+		}
+		for (int k : a)
+			System.out.print(k + " ");
+
 	}
 
 }
