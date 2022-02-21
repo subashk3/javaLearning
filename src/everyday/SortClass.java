d173cef1d4b1ca3d9bc7aacd9e5c99bf05418103
subashk3 <subashktsmpi@gmail.com>
2022-02-21 07:09:24 +0530

    upload

--- src/everyday/SortClass.java
+++ src/everyday/SortClass.java
@@ -21,6 +21,7 @@ public class SortClass {
 			System.out.print(k + " ");
 
 	}
+
 	public static void main(String[] args) {
 		int a[] = { 40, 7, 5, 1 };
 		int n = a.length;
@@ -28,20 +29,39 @@ public class SortClass {
 		SortClass o = new SortClass();
 		// o.bubbleSorting(a, n);
 		o.selectionSorting(a, n);
+		System.err.println();
+		o.insertSorting(a, n);
 	}
+
+	public void insertSorting(int[] a, int n) {
+		for (int i = 0; i < n; i++) {
+			int temp = a[i];
+			int j = i;
+			while (j > 0 && temp < a[j - 1]) {
+				a[j]= a[j-1];
+
+				j--;
+			}
+
+		}
+		for (int k : a)
+			System.out.print(k + " ");
+
+	}
+
 	public void selectionSorting(int[] a, int n) {
-		int min;
+
 		for (int i = 0; i < n - 1; i++) {
-			min = i;
 			for (int j = i + 1; j < n; j++) {
-				if (a[min] > a[j]) {
-					min = j;
+				if (a[i] > a[j]) {
+					int temp = a[j];
+					a[j] = a[i];
+					a[i] = temp;
 				}
 			}
-			int temp = a[min];
-			a[min] = a[i];
-			a[i] = temp;
-
+			/*
+			 * int temp = a[min]; a[min] = a[i]; a[i] = temp;
+			 */
 		}
 		for (int k : a)
 			System.out.print(k + " ");
