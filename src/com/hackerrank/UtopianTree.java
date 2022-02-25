b69d90c93e1c135823494157ae9a6c51e5ba70e3
subashk3 <subashktsmpi@gmail.com>
2022-02-25 20:53:55 +0530

    Upload

--- src/com/hackerrank/UtopianTree.java
+++ src/com/hackerrank/UtopianTree.java
@@ -1,3 +1,55 @@
+/*The Utopian Tree goes through 2 cycles of growth every year. Each spring, it doubles in height. Each summer, its height increases by 1 meter.
+
+A Utopian Tree sapling with a height of 1 meter is planted at the onset of spring. How tall will the tree be after  growth cycles?
+
+For example, if the number of growth cycles is , the calculations are as follows:
+
+Period  Height
+0          1
+1          2
+2          3
+3          6
+4          7
+5          14
+Function Description
+
+Complete the utopianTree function in the editor below.
+
+utopianTree has the following parameter(s):
+
+int n: the number of growth cycles to simulate
+Returns
+
+int: the height of the tree after the given number of cycles
+Input Format
+
+The first line contains an integer, , the number of test cases.
+ subsequent lines each contain an integer, , the number of cycles for that test case.
+
+Constraints
+
+
+
+Sample Input
+
+3
+0
+1
+4
+Sample Output
+
+1
+2
+7
+Explanation
+
+There are 3 test cases.
+
+In the first case (), the initial height () of the tree remains unchanged.
+
+In the second case (), the tree doubles in height and is  meters tall after the spring cycle.
+
+In the third case (), the tree doubles its height in spring (, ), then grows a meter in summer (, ), then doubles after the next spring (, ), and grows another meter after summer (, ). Thus, at the end of 4 cycles, its height is  meters.*/
 package com.hackerrank;
 
 import java.util.*;
@@ -9,14 +61,13 @@ public class UtopianTree {
 		int h = 1;
 
 		for (int i = 0; i < n; i++) {
-
 			if (h % 2 != 0) {
-
 				h = h * 2;
 			} else
 				h += 1;
-
 		}
+
+		
 		// System.out.println(h);
 
 		return h;
@@ -24,6 +75,9 @@ public class UtopianTree {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		
+		
+		
 		int t = sc.nextInt();
 
 		while (t-- > 0) {
