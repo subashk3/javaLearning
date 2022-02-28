87a29aa9d0e95361019e4a67a496113c62914009
subashk3 <subashktsmpi@gmail.com>
2022-02-28 22:55:28 +0530

    Upload

---
+++ src/oops/VolatileClass.java
@@ -0,0 +1,39 @@
+package oops;
+
+public class VolatileClass {
+
+	private static volatile int COUNTER = 0;
+
+	public static void main(String[] args) {
+		new ReadingThread().start();
+		new WritingThread().start();
+	}
+
+	static class ReadingThread extends Thread {
+
+		@Override
+		public void run() {
+			while (COUNTER < 4) {
+
+			}
+			System.out.println("Counter reached 4.");
+
+		}
+
+	}
+
+	static class WritingThread extends Thread {
+
+		@Override
+		public void run() {
+			while (COUNTER < 10) {
+				COUNTER++;
+				System.out.println("Counter incremented to " + COUNTER);
+
+			}
+
+		}
+
+	}
+
+}
