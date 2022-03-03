f3f14d7c3a018f0afa67728cac0f0b08aa063502
subashk3 <subashktsmpi@gmail.com>
2022-03-03 17:22:05 +0530

    Upload

--- src/com/hackerrank/BreakingRecords.java
+++ src/com/hackerrank/BreakingRecords.java
@@ -81,10 +81,7 @@ public class BreakingRecords {
 			}
 
 		}
-
-		
 		for (int i = 0; i < 2; i++) {
-
 			System.out.print(result[i]+" ");
 		}
 
---
+++ src/testprograms/SummaOruClass.java
@@ -0,0 +1,13 @@
+package testprograms;
+
+public class SummaOruClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x =5;
+		String y ="John";
+		System.out.println(x+y);
+
+	}
+
+}
