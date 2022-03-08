f63df99d22364b524c971025ce222a00e187aa42
subashk3 <subashktsmpi@gmail.com>
2022-03-08 15:11:32 +0530

    upload

---
+++ src/com/hackerrank/ExtraLongFactorials.java
@@ -0,0 +1,27 @@
+package com.hackerrank;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class ExtraLongFactorials {
+	public static void extraLongFactorials(int n) {
+		
+		BigInteger b = BigInteger.valueOf(1);
+		
+		for(int i=2;i<=n;i++) {
+			
+			b = b.multiply(b.valueOf(i));
+		}
+
+		System.out.println(b);
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+
+		extraLongFactorials(n);
+	}
+
+}
